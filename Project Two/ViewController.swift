0b452ac1f1c7247aa92a58488f865d476c34c2c7
NoahNua <NoahNua@hawaii.edu>
2021-02-25 10:12:39 -1000

    Initial Commit

---
+++ Project Two/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project Two
+//
+//  Created by Noah Nua on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
