5428a89284793ecb7aee7028e944e188558c7497
NoahNua <NoahNua@hawaii.edu>
2021-03-04 11:55:43 -1000

    Tab 1 View Controller User Interface Completed

--- Project Two/ViewController.swift
+++ Project Two/ViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var selfPortrait: UIImageView!
+   
+    @IBOutlet weak var uhWest: UIImageView!
+    
+    @IBOutlet weak var myName: UILabel!
+    
+    @IBOutlet weak var nameName: UILabel!
+   
+    @IBOutlet weak var myMajor: UILabel!
+    
+    @IBOutlet weak var majorMajor: UILabel!
+    
+    @IBOutlet weak var mySpring: UILabel!
+    
+    @IBOutlet weak var textBox: UITextView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        title = "About Me"
+  
+        selfPortrait.image = UIImage(named:"Self Portrait")
+//        ^sets imiage of me
+        
+        uhWest.image = UIImage(named:"UHWO")
+//        sets UHWO symbol
+        
+        self.myName.text = "My Name is:"
+        
+        self.nameName.text = "Noah"
+        
+        self.myMajor.text = "My Major is:"
+        
+        self.majorMajor.text = "SCFS Program"
+        
+        self.mySpring.text = "My Spring 2021 Classes Are"
+        
+        self.textBox.text = "Is overwelhming with seven classes, but I am determind to graduate this year"
+        
     }
 
 
