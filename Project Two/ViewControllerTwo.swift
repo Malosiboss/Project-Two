10b13811c80988ecfd6f5c5d7a64c49f71bbaafd
NoahNua <NoahNua@hawaii.edu>
2021-03-04 16:46:59 -1000

    add code for weather converter but app keeps failing

--- Project Two/ViewControllerTwo.swift
+++ Project Two/ViewControllerTwo.swift
@@ -9,22 +9,36 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
 
+    @IBOutlet weak var setLabel: UILabel!
+
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-
-        // Do any additional setup after loading the view.
+        title = "Weather Converter"
+        setLabel.text = ""
+    
     }
+
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        var convertedNumber = Int(textField.text!)!
+        convertedNumber = (convertedNumber - 32) / Int(1.8)
+        setLabel.text = "\(convertedNumber)"
+        
+            
+            
+        
+        
+        
+        
+    }
+  
     
 
-    /*
-    // MARK: - Navigation
+  
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
+
